83dc3264c3cf59ccd559682d01dc8b3fcab27469
zygis <zygimantas@benetis.me>
2021-08-15 19:45:50 +0300

    wip(enums) defining possible data model

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,14 @@
 @main def hello: Unit = 
   println("Hello world!")
-  println(msg)
 
-def msg = "I was compiled by Scala 3. :)"
+enum HomeAccessory:
+  case AutomaticFrontGates extends HomeAccessory
+  case FrontCamera extends HomeAccessory
+  case GateDoorbell extends HomeAccessory
+  case SmartLight(light: Light) extends HomeAccessory
+end HomeAccessory
+
+enum Light:
+  case LightByTimer extends Light
+  case LightByEvent extends Light
+end Light
